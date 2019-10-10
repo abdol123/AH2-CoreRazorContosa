9e64bcf40a93ae6aaf20e486ff931db3b52d3f84
21706644 <21706644@HSPD15083493>
2019-10-10 11:42:58 +0100

    Add project files.

---
+++ TH2_CoreRazor-Contosa/Pages/Hello.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TH2_CoreRazor_Contosa.Pages
+{
+    public class HelloModel : PageModel
+    {
+
+        public string UserName { get; set; }
+
+        public void OnGet()
+        {
+            UserName = null;
+        }
+
+        public void OnPost()
+        {
+            this.UserName = Request.Form["UserName"];
+        }
+    }
+}
+    
